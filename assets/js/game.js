6680eb68a6ee4be8cf5a8837d4407b5d96fcb08b
Matthew Wiser <matt.wiser@icloud.com>
2022-02-22 16:38:27 -0600

    created initial functions to capture player name and initiate a fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+// This captures the player's name for their robot
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+// This creates a function called fight
+function fight(){
+    window.alert("The fight has begun!");
+};
+
+// fight();
