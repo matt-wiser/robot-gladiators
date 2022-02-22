fdcbcf13774186eebb2877490283daff7a666553
Matthew Wiser <matt.wiser@icloud.com>
2022-02-22 17:05:02 -0600

    created beginning game logic for attacks and logging results

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,39 @@
-// This captures the player's name for their robot
+// This captures the player's name for their robot and sets initial health and attack values
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
 
-// This creates a function called fight
-function fight(){
-    window.alert("The fight has begun!");
+// This establishes starting values for enemy name, health, and attack
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function(){
+    // Alert players they are starting a round
+    window.alert("Welcome to Robot Gladiators!");
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+    // Check enemy's health
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health remaining!");
+    }
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    // Check player's health
+    if (playerHealth <= 0){
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health remaining!");
+    }
 };
 
-// fight();
+fight();
